68ed5696a3a81446a1ba6a5bf0faa40efed7e8bd
Ankita Verma <ankita.verma@geminisolutions.in>
2021-06-15 19:40:37 +0530

    added edit blog

--- src/main/java/com/tutorialproject/controller/BlogsController.java
+++ src/main/java/com/tutorialproject/controller/BlogsController.java
@@ -2,10 +2,18 @@ package com.tutorialproject.controller;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tutorialproject.model.BlogsModel;
@@ -30,5 +38,26 @@ public class BlogsController
 		return blogrepo.findByBlogid(id);
 		
 	}
-
+	@Transactional
+	@DeleteMapping("blog/delete/{id}")
+	public ResponseEntity<?> deleteBlog(@PathVariable("id") String blogid)
+	{
+		System.out.println(blogid);
+			blogrepo.deleteByBlogid(blogid);
+			return new ResponseEntity(202,HttpStatus.OK);
+	}
+	@PostMapping("insert")
+	public ResponseEntity postBlog(@RequestBody BlogsModel bmodel)
+	{
+		 blogrepo.save(bmodel);
+		 return new ResponseEntity(200,HttpStatus.OK);
+	}
+	
+	@Transactional
+	@PutMapping("edit/blog/{id}")
+	public ResponseEntity<?> editBlog(@RequestBody BlogsModel bm)
+	{
+		blogrepo.updateBlog(bm.getHeading(), bm.getContent(), bm.getDetailBlog(), bm.getBlog_id());
+		return new ResponseEntity(200,HttpStatus.OK);
+	}
 }
--- src/main/java/com/tutorialproject/repository/BlogsRepo.java
+++ src/main/java/com/tutorialproject/repository/BlogsRepo.java
@@ -1,6 +1,8 @@
 package com.tutorialproject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.tutorialproject.model.BlogsModel;
@@ -8,5 +10,13 @@ import com.tutorialproject.model.BlogsModel;
 public interface BlogsRepo extends JpaRepository<BlogsModel, Integer>{
 
 	BlogsModel findByBlogid(String id);
+
+	@Modifying
+	@Query(value="delete from BlogsModel bm where bm.blogid =?1")
+	void deleteByBlogid(String blogid);
+	
+	@Modifying
+	@Query(value="update blogs_model bm set bm.heading=?1 and bm.content=?2 and bm.detail_blog=?3 where bm.blogid=?4",nativeQuery = true)
+	void updateBlog(String heading,String content,String detailBlog,String blogid);
 
 }
